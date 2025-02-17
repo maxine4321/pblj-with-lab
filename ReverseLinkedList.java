a61d93d9d0dd8e8ea4a5226ea7eaa88dfcdd116a
Mehak Chauhan <chauhanmehak465@gmail.com>
2025-02-17 10:55:22 +0530

    2nd file

---
+++ ReverseLinkedList.java
@@ -0,0 +1,43 @@
+
+public class ReverseLinkedList {
+    public static class Node {
+        int data;
+        Node next;
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+        System.out.println("Original Linked List:");
+        printList(head);
+        head = reverse(head);
+        System.out.println("Reversed Linked List:");
+        printList(head);
+    }
+}
